5511bdd3350696e0a45fa20d345269fd4ac71fe6
Dan Clawson <clawson@uen.org>
2015-03-14 08:30:20 -0600

    Initial Commit

---
+++ Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Dan Clawson on 3/14/15.
+//  Copyright (c) 2015 Slacker Tools. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
